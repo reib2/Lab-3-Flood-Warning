ed024689ebc25e539d1c0f055305161961e9ddbe
Ria Bacharach <riabac876@gmail.com>
2022-01-31 19:22:53 +0000

    Testing and attempting task 1B

--- floodsystem/ria_testing.py
+++ floodsystem/ria_testing.py
@@ -1,10 +1,10 @@
-# Copyright (C) 2018 Garth N. Wells
-#
-# SPDX-License-Identifier: MIT
-"""This module contains a collection of functions related to
-geographical data.
+from haversine import haversine, Unit
+    
+lyon = (45.7597, 4.8422) # (lat, lon)
+paris = (48.8567, 2.3508)
 
-"""
+print (haversine(lyon, paris))
+ 
 
 from utils import sorted_by_key  # noqa
 from stationdata import build_station_list
@@ -12,5 +12,6 @@ from stationdata import build_station_list
 
 def stations_by_distance(): 
     """This module sorts stations by distance and returns a list of (station, distance) tupules."""
+    station = build_station_list()
 
 stations_by_distance()
