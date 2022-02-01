335f0b49c4e8ce4156dfb37e20026305910692e3
Ria Bacharach <riabac876@gmail.com>
2022-02-01 22:23:01 +0000

    1B complete, need to add tests

--- Task1B.py
+++ Task1B.py
@@ -1,11 +1,18 @@
+"""Task 1B: this programme is used to demonstrate the build_station_list 
+function in the geo.py module."""
+
+#imports
 from floodsystem.stationdata import build_station_list
 from floodsystem.geo import stations_by_distance
 
-#builds list of stations
-stations = build_station_list()
+stations = build_station_list() #builds list of stations
 
-#distance to measure from 
-p = (52.2053,0.1218)
+p = (52.2053,0.1218) #distance to measure from 
 
-#returns list of stations sorted by distance 
-stations_by_distance(stations, p)
+stations_distance = stations_by_distance(stations, p) #returns list of stations sorted by distance 
+
+closest = stations_distance[:10] #slices list to include the 10 locations closest to p 
+furthest = stations_distance[-10:] #slices list to include the 10 locations furthest from p 
+
+print ("The ten stations closest to Cambridge City Centre are: " + str(closest))
+print ("The ten stations furthest from Cambridge City Centre are: " + str(furthest))
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -10,23 +10,20 @@ from .utils import sorted_by_key  # noqa
 from floodsystem.stationdata import build_station_list
 
 
-
 def stations_by_distance(stations, p): 
-    """This module sorts stations by distance and returns a list of (station, distance) tupules."""
-    from haversine import haversine, Unit
- 
+    """This module sorts stations by distance and returns a 
+    list of (station, town, distance) tupules."""
+   
+    from haversine import haversine, Unit #import haversine function from library
 
-    # Print number of stations
-    print("Number of stations: {}".format(len(stations)))
-
-    # Display data from 3 stations:
+    list_station_dist = [] #initiates list to store stations and distance 
+      
+    #iterate through stations and calculate distamces
     for station in stations:
-        if station.name in ['Cambridge Jesus Lock']:
-            coord = station.coord
-            distance = haversine(coord, p)
-            print (distance)
+        distance = haversine(station.coord, p) #use haversine function to calculate distance between station and p  
+        list_station_dist.append((station.name, station.town, distance)) #add data to list
+    
+    sorted_list = sorted_by_key(list_station_dist, 2) #use sorting module to sort by distance
 
-
-    #print (haversine(lyon, paris))
-
+    return sorted_list 
 
