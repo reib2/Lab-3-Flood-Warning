446111aff318ea7b8eae02882a654d80fcab2f22
Ria Bacharach <riabac876@gmail.com>
2022-02-02 20:45:58 +0000

    started task IF added method to monitoring station

--- floodsystem/station.py
+++ floodsystem/station.py
@@ -38,3 +38,13 @@ class MonitoringStation:
         d += "   river:         {}\n".format(self.river)
         d += "   typical range: {}".format(self.typical_range)
         return d
+    
+    def typical_range_consistent(self):
+        if self.typical_range == ():
+            return False 
+        elif self.typical_range(0) > self.typical_range(1):
+            return False 
+        else:
+            return True 
+    
+
--- floodsystem/stationdata.py
+++ floodsystem/stationdata.py
@@ -87,3 +87,13 @@ def update_water_levels(stations):
         if station.measure_id in measure_id_to_value:
             if isinstance(measure_id_to_value[station.measure_id], float):
                 station.latest_level = measure_id_to_value[station.measure_id]
+
+
+#incomplete for task 1F...
+def inconsistent_typical_range_stations(stations): 
+
+    inconsistent_stations = []
+
+    for station in stations: 
+        if MonitoringStation.typical_range_consistent == False:
+            inconsistent_stations.apped 
