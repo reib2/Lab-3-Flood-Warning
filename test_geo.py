23e22ced40beadc2fc347c4a0f7807f2dce422dd
Ria Bacharach <riabac876@gmail.com>
2022-02-06 13:23:21 +0000

    Testing tests

---
+++ test_geo.py
@@ -0,0 +1,19 @@
+"""Unit test for sort_by_distance function in the geo module"""
+
+import floodsystem.geo 
+from floodsystem.stationdata import build_station_list
+
+
+def test_stations_by_distance():
+    stations = build_station_list()
+
+
+
+
+#test haversine 
+
+#test sort order 
+
+#test sort order is high to low 
+
+
