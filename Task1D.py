9c347a67e9042bd0afa3db8eecd730d9467cb10b
Connie Jordan <cfmj2@cam.ac.uk>
2022-02-07 13:47:05 +0000

    1D part 2

--- Task1D.py
+++ Task1D.py
@@ -1,5 +1,6 @@
 from floodsystem.stationdata import build_station_list
 from floodsystem.geo import rivers_with_station
+from floodsystem.geo import stations_by_river
 
 stations = build_station_list()
 rivers = rivers_with_station(stations)
@@ -8,3 +9,15 @@ first = list(sorted(rivers))[:10]
 
 
 print(number, 'stations. First 10 -', first)
+
+river_stations = stations_by_river(stations)
+
+river_names = ['River Aire', 'River Cam', 'River Thames']
+sorted_stations = []
+
+for i in river_names:
+    river = i
+    sorted_stations = river_stations[i]
+    sorted_stations.sort()
+
+    print(river, sorted_stations)
