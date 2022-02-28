2cbdabfe944dd22e854b8fc5ed874b184b91c646
Ria Bacharach <riabac876@gmail.com>
2022-02-28 08:42:19 +0000

    2G finished

--- Task2G.py
+++ Task2G.py
@@ -1,69 +1,18 @@
-import matplotlib.pyplot as plt
-from floodsystem.flood import stations_level_over_threshold
-from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.stationdata import build_station_list
-import matplotlib
-import numpy as np
-import datetime
-from floodsystem.plot import plot_water_levels, plot_water_level_with_fit
 from floodsystem.stationdata import update_water_levels
 from floodsystem.flood import stations_highest_rel_level
-from floodsystem.station import MonitoringStation
-from floodsystem.analysis import polyfit
-
+from floodsystem.flood import flood_warning
 
 #build list and splice
 stations = build_station_list() 
 update_water_levels(stations)
-stations = stations[:40]
+#stations_list = stations[:50]
+
+N = 40
+top_N = stations_highest_rel_level(stations, N)
+station_list = []
+for station in top_N: 
+    station_list.append(station[0])
 
 
-dt = 2
-at_risk =[]
-risk = ""
-
-for station in stations:
-    #print (station.name)
-    dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
-    p=1
-
-    poly, d0 = polyfit(dates, levels, p)
-
-    #print (poly)
-    if poly != None: 
-        slope = poly[1]
-
-        fraction = station.relative_water_level()
-
-        if slope > 0:
-
-            if fraction != None: 
-                if fraction < 0.8:
-                    risk = "LOW"
-                elif fraction < 1: 
-                    risk = "MODERATE"
-                elif fraction < 1.5:
-                    risk = "HIGH"
-                else: 
-                    risk = "SEVERE"
-            
-            print (str(station.name) + " " + str(risk)) 
-
-            #at_risk.append((station.name, risk))
-
-        else:
-            if fraction != None: 
-                if fraction < 1:
-                    risk = "LOW"
-                elif fraction < 2: 
-                    risk = "MODERATE"
-                elif fraction < 5:
-                    risk = "HIGH"
-                else: 
-                    risk = "SEVERE"
-
-            print (str(station.name) + " " + str(risk))
-            #at_risk.append((station.name, risk))
-        
-        
-
+flood_warning(station_list)
--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,6 +1,12 @@
 from .station import MonitoringStation
 from .utils import sorted_by_key
 
+from floodsystem.datafetcher import fetch_measure_levels
+import numpy as np
+import datetime
+from floodsystem.analysis import polyfit
+
+
 
 def stations_level_over_threshold(stations, tol): 
     """This function returns a list of stations where water
@@ -38,6 +44,63 @@ def stations_highest_rel_level(stations, N):
     return top_N
 
 
+
+def flood_warning(station_list):
+    "Function issues risk level for each station."
+
+    dt = 2
+    at_risk =[]
+    risk = ""
+
+    for station in station_list:
+        #print (station.name)
+        dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt)) #return levels for last two days 
+
+    
+        p=1
+
+        try:
+
+            poly, d0 = polyfit(dates, levels, p)
+
+            #print (poly)
+            if poly != []: 
+                slope = poly[1]
+
+                fraction = station.relative_water_level()
+
+                if slope > 0: #increasing water levels
+
+                    if fraction != None: 
+                        if fraction < 0.8:
+                            risk = "LOW"
+                        elif fraction < 1: 
+                            risk = "MODERATE"
+                        elif fraction < 1.5:
+                            risk = "HIGH"
+                        else: 
+                            risk = "SEVERE"
+                    
+                    print (str(station.name) + " " + str(risk)) 
+
+                    #at_risk.append((station.name, risk))
+
+                else: #decreasing water levels
+                    if fraction != None: 
+                        if fraction < 1:
+                            risk = "LOW"
+                        elif fraction < 2: 
+                            risk = "MODERATE"
+                        elif fraction < 5:
+                            risk = "HIGH"
+                        else: 
+                            risk = "SEVERE"
+
+                    print (str(station.name) + " " + str(risk))
+                    #at_risk.append((station.name, risk))
+        except:
+                print (str(station.name) + " Missing data")
+                
 
 
 
